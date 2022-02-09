e339f2276bbd29f2391074d344653169fd50ccdb
Horace Williams <horace.d.williams@gmail.com>
2022-02-09 15:37:03 -0800

    WIP adding some leaflet

--- viewer.tsx
+++ viewer.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import { deserialize } from 'flatgeobuf';
 import { deserializeFiltered } from 'flatgeobuf/lib/mjs/generic/featurecollection';
 import { Rect } from 'flatgeobuf/lib/mjs/packedrtree';
 import { HttpReader } from 'flatgeobuf/lib/mjs/HttpReader';
 import HeaderMeta from 'flatgeobuf/lib/mjs/HeaderMeta';
 import { GeometryType } from 'flatgeobuf/lib/mjs/generic';
+import L from 'leaflet';
 
 const GeomTypes = {
   0: "Unknown",
@@ -32,14 +33,32 @@ function geomType(num: number): GeometryType {
   return GeomTypes[num];
 }
 
-const Viewer = (props: { url: string, header: HeaderMeta }) => (
-  <div>
-    <h1>View Url</h1>
-    <h2>{props.url}</h2>
-    <p>Num Features: {props.header.featuresCount}</p>
-    <p>Geometry Type: {geomType(props.header.geometryType)}</p>
-  </div>
-);
+const Viewer = (props: { url: string, header: HeaderMeta }) => {
+  const mapEl = useRef(null);
+  const [map, updateMap] = useState(null);
+
+  useEffect(() => {
+    console.log('my effect', mapEl);
+    const map = L.map(mapEl.current).setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    console.log('mounted map', map);
+    updateMap(map);
+  }, []);
+
+  return (
+    <div>
+      <h1>View Url</h1>
+      <h2>{props.url}</h2>
+      <p>Num Features: {props.header.featuresCount}</p>
+      <p>Geometry Type: {geomType(props.header.geometryType)}</p>
+      <div id="map" style={{ height: '400px', width: '600px' }} ref={mapEl}>
+      </div>
+    </div>
+  );
+}
 
 const url = new URLSearchParams(window.location.search).get("url");
 if (url == null) {
